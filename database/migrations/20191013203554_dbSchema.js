4719c2b681715e62a56d17d0e6e2a6be5584c4e6
Jeff Butler <jeff.butler.springdale@gmail.com>
2019-10-14 11:52:37 -0500

    user_recipes table added

--- database/migrations/20191013203554_dbSchema.js
+++ database/migrations/20191013203554_dbSchema.js
@@ -13,7 +13,27 @@ exports.up = function(knex) {
             .string('email', 60)
             .unique();
       })
-      
+      .createTable('user_recipes', user_recipes => {
+        user_recipes.increments();
+
+        user_recipes
+            .string('title', 255)
+            .notNullable()
+        user_recipes
+            .binary('public_private')
+        user_recipes
+            .integer('water_temp');
+        user_recipes   
+            .integer('user_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE');
+        user_recipes
+            .string('coarseness')
+    })
 };
 
 exports.down = function(knex) {
