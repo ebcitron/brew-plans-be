af95248992691154b1f52176f23766b853d80bab
Jeff Butler <jeff.butler.springdale@gmail.com>
2019-10-14 16:15:06 -0500

    baseline testing for users and ingredients

--- database/models_routers/users/users.spec.js
+++ database/models_routers/users/users.spec.js
@@ -1,3 +1,6 @@
+const request = require('supertest');
+const server = require('../../../api/server.js');
+
 describe('the server', () => {
     describe('GET /', () => {
         it('should run the testing env', () => {
