8e686aefd4cee82c969309ce186276096f97145e
Jeff Butler <jeff.butler.springdale@gmail.com>
2019-10-15 12:10:30 -0500

    users base db functions and routes

--- database/models_routers/users/usersModel.js
+++ database/models_routers/users/usersModel.js
@@ -2,7 +2,9 @@ const db = require('../../dbConfig.js');
 
 module.exports = {
     findAllUsers,
-    findById
+    findById,
+    add,
+    removeUser
   };
   
   function findAllUsers() {
@@ -13,4 +15,16 @@ module.exports = {
     return db('users')
       .where({ id })
       .first();
+}
+
+  async function add(user) {
+  const [id] = await db('users').insert(user);
+
+  return findById(id);
+}
+
+  function removeUser(id) {
+    return db('users')
+      .where({ id })
+      .del()
 }
--- database/models_routers/users/usersRouter.js
+++ database/models_routers/users/usersRouter.js
@@ -25,4 +25,20 @@ router.get('/:id', (req, res) => {
     });
   });
 
+router.delete('/:id', (req, res) => {
+    const { id }  = req.params; 
+  
+    Users.removeUser(id)
+    .then(deleted => {
+      if (deleted) {
+        res.json({ removed: deleted });
+      } else {
+        res.status(404).json({ message: 'Could not find User with given id' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to delete User' });
+    });
+  });
+
 module.exports = router;
