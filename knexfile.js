eea5ff3bd92363b1638a8d4306dae1d21eb4c618
Jeff Butler <jeff.butler.springdale@gmail.com>
2019-10-17 14:10:21 -0500

    testing to see if removing proc works

--- knexfile.js
+++ knexfile.js
@@ -1,37 +1,25 @@
 module.exports = {
   
-  // development: {
-  //   client: 'sqlite3',
-  //   useNullAsDefault: true,
-  //   connection: { filename: './database/data.db3' },
-  //   migrations: {
-  //     directory: './database/migrations',
-  //     tableName: 'dbmigrations',
-  //   },
-  //   seeds: { directory: './database/seeds' },
-  // },
-
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    connection: { filename: './database/data.db3' },
     migrations: {
-        directory: __dirname + '/database/migrations',
-      },
-      
-    seeds: {
-        directory: __dirname + '/database/seeds',
-      },
+      directory: './database/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './database/seeds' },
   },
 
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-        directory: __dirname + '/database/migrations',
-      },
+  // production: {
+  //   client: 'pg',
+  //   connection: process.env.DATABASE_URL,
+  //   migrations: {
+  //       directory: __dirname + '/database/migrations',
+  //     },
       
-    seeds: {
-        directory: __dirname + '/database/seeds',
-      },
-  },
+  //   seeds: {
+  //       directory: __dirname + '/database/seeds',
+  //     },
+  // },
 };
