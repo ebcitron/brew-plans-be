749e4c18e4365b201cbf9d84c40ce5ca013b9c7e
Jeff Butler <jeff.butler.springdale@gmail.com>
2019-10-17 11:45:22 -0500

    testing deploy fix for dev 2

--- knexfile.js
+++ knexfile.js
@@ -2,14 +2,14 @@
 
 module.exports = {
   development: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: { filename: './database/data.db3' },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations',
-    },
-    seeds: { directory: './database/seeds' },
+        directory: __dirname + '/database/migrations',
+      },
+    seeds: {
+        directory: __dirname + '/database/seeds',
+      },
   },
 
   production: {
