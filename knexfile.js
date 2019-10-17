bb1163115ff08ef075ad128e23acf644ebf8203b
Jeff Butler <jeff.butler.springdale@gmail.com>
2019-10-17 14:40:56 -0500

    testing fix for proc file addition

--- knexfile.js
+++ knexfile.js
@@ -1,25 +1,32 @@
 module.exports = {
   
   development: {
-    client: 'pg',
+    client: 'sqlite3',
     useNullAsDefault: true,
-    connection: { filename: './database/data.db3' },
+    connection: { 
+      filename: './database/data.db3'
+     },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
     migrations: {
       directory: './database/migrations',
       tableName: 'dbmigrations',
     },
     seeds: { directory: './database/seeds' },
   },
-
-  // production: {
-  //   client: 'pg',
-  //   connection: process.env.DATABASE_URL,
-  //   migrations: {
-  //       directory: __dirname + '/database/migrations',
-  //     },
+  
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+        directory: __dirname + '/database/migrations',
+      },
       
-  //   seeds: {
-  //       directory: __dirname + '/database/seeds',
-  //     },
-  // },
+    seeds: {
+        directory: __dirname + '/database/seeds',
+      },
+  },
 };
